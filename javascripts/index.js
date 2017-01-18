ccf619d212612e7083afcc835fbb77cafcf89910
ztao <yushenxin@foxmail.com>
2017-01-18 20:54:44 +0800

    20170118-6

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,8 +3,11 @@ $(function(){
 	/*全局变量*/
 	var $back2top=$("a#back-to-top");
 	var $window=$(window);
-	
+	var $navTop=$("nav").offset().top;
 	/*事件绑定*/
+	$window.resize(function(){
+		$navTop=$("nav").offset().top;
+	});
 	$window.scroll(fScroll);
 	
 	$back2top.click(function(){
@@ -19,7 +22,7 @@ $(function(){
 	/*函数封装*/
 	function fScroll(){
 		//console.log($(window).scrollTop());
-		if($window.scrollTop()>500)
+		if($window.scrollTop()>$navTop)
 			$back2top.css("display","block");
 		else 
 			$back2top.css("display","none");
